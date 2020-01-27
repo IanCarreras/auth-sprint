ec9a7e2adae146177d35468d682e9a13cd529942
Ian <mus5002@aol.com>
2020-01-26 19:42:26 -0500

    added users model, secrets configuration, auth middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,46 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets')
+const usersModel = require('./users-model')
 const router = require('express').Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+router.post('/register', async (req, res, next) => {
+  try {
+    const saved = await usersModel.add(req.body)
+
+    res.status(201).json(saved)
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post('/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body
+    const user = await usersModel.findBy({ username }).first()
+
+    const passwordValid = await bcrypt.compare(password, user.password)
+
+    if (user && passwordValid) {
+      const token = jwt.sign({
+        subject: user.id,
+        username: user.username
+      }, secrets.jwt, {
+        expiresIn: '7d'
+      })
+
+      res.status(200).json({
+        message: `welcome ${user.username}`,
+        token: token
+      })
+    } else {
+      res.status(401).json({
+        message: 'invalid credentials'
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
 });
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,18 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken')
+const secrets = require('../config/secrets.js')
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+module.exports = () => {
+  return (req, res, next) => {
+    try {
+      const token = req.headers.authorization
+      const decoded = jwt.verify(token, secrets.jwt)
+
+      req.userId = decoded.subject
+      next()
+    } catch (err) {
+      return res.status(401).json({
+        message: 'invalid credentials'
+      })
+    }
+  } 
 };
